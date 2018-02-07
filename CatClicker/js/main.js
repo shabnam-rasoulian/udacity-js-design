78c1785340af436fc3491d90ee6601d41a9b7c51
Shabnam Rasoulian <shabnam.rasoulian@gmail.com>
2018-02-07 13:02:41 -0500

    Implement cat clicker 3.

--- CatClicker/js/main.js
+++ CatClicker/js/main.js
@@ -7,24 +7,46 @@ let cats = [{
 		"name": "Jessy",
 		"count": 0,
 		"image_url": "images/cat2.jpg"
+	}, {
+		"name": "Tommy",
+		"count": 0,
+		"image_url": "images/cat3.jpg"
+	}, {
+		"name": "Sippy",
+		"count": 0,
+		"image_url": "images/cat4.jpg"
+	}, {
+		"name": "Rusty",
+		"count": 0,
+		"image_url": "images/cat5.jpg"
 }];
 
-function display(items) {
-	let $container = $("#container");
-	$container.empty();
+function displayItem(item) {
+	let $display = $("#display");
+	$display.empty();
+	let $htmlName = $("<h3>" + item.name + "</h3>");
+	$display.append($htmlName);
+	$cat_image = $("<img src='" + item.image_url + "' alt='A cat' width='500' height='300'>");
+	$display.append($cat_image);
+	$display.append("<div id='count'>" + item.count + "</div>");
+	$cat_image.click(function() {
+		item.count++;
+		$("#count").text(item.count);
+	});
+};
+
+function displayList(items) {
+	let $list = $("#list");
+	$list.empty();
 	for (let i = 0; i < items.length; i++) {
-		let $cat_elem = $("<div class='cat_elem'></div>");
-		$container.append($cat_elem);
-		$cat_elem.append("<div>" + items[i].name + "</div>");
-		$cat_image = $("<img src='" + items[i].image_url + "' alt='A cat' width='500' height='300' id='cat_image" + i + "'>");
-		$cat_elem.append($cat_image);
-		$("#cat_image" + i + "").click(function() {
-			items[i].count++;
-			$("#count" + i + "").text(items[i].count);
-		});
-		$count = $("<p id='count" + i + "'>0</p>")
-		$cat_elem.append($count);
+		let $item = $("<li><h3>" + items[i].name+ "</h3></li>");
+		$item.click((function(item) {
+			return function() {
+				displayItem(item);
+			};
+		}(items[i])));
+		$list.append($item);
 	}
 };
 
-display(cats);
+displayList(cats);
